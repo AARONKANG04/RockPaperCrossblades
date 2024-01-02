bb8f3cdc384244a0a60a51dcc23d15871e250270
Aaron Kang <aaron.h.kang@icloud.com>
2024-01-01 17:48:27 -0800

    new cpp file

---
+++ rpc.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib> // for rand() and srand()
+#include <ctime>   // for time()
+
+int main() {
+    // Seed the random number generator with the current time
+    std::srand(std::time(0));
+
+    // Generate a random number between 1 and 100
+    int randomNumber = std::rand() % 100 + 1;
+
+    // Print the random number
+    std::cout << "Random Number: " << randomNumber << std::endl;
+
+    return 0;
+}
+
